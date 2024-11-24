704c9ea878bd9efc8b5dab3eda493190d08d7a37
mehrdadtorki <mehrdadtorki1381@gmail.com>
2024-11-24 16:53:11 +0330

    feat: change Nextyjs to react and new UI

--- middleware.ts
+++
@@ -1,2 +0,0 @@
-// The most simple usage is when you want to require authentication for your entire site. You can add a middleware.js file with the following:
-export { default } from "next-auth/middleware";
